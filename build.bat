c4036e261089699c1ec3f402ba9c6ed4c807e729
William Roe <git@wjlr.org.uk>
2024-04-16 18:18:16 +0100

    One day I'll work out how to do this

--- build.bat
+++ build.bat
@@ -9,6 +9,11 @@ C:\dev\odin\dev-master\odin build src ^
     -debug ^
     -show-timings
 
+rem echo Web build
+rem C:\dev\odin\dev-master\odin build src ^
+rem     -target:js_wasm32 ^
+rem     -show-timings
+
 if NOT "%1"=="all" goto End
 
 echo Release build
