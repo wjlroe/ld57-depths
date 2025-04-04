41742058a523fee0f81963fd1d7e2ea127d737b9
William Roe <git@wjlr.org.uk>
2025-04-04 23:41:16 +0100

    add build-and-run mode to Windows build script

--- build.bat
+++ build.bat
@@ -1,28 +1,45 @@
 @echo off
 setlocal
 
+SET PATH=%PATH%;C:\dev\odin\2025-04
+
+set build_release=no
+set run_after_build=no
+
+if "%1"=="run" set run_after_build=yes
+if "%1"=="all" set build_release=yes
+
 echo Debug build
-C:\dev\odin\dev-master\odin build src ^
-    -out:build/base_code.exe ^
+odin build src ^
+    -out:build/base_code_debug.exe ^
     -build-mode:exe ^
     -subsystem:console ^
     -debug ^
-    -show-timings
+    -o:minimal ^
+    -show-timings || goto :error
+
+if "%run_after_build%"=="yes" .\build\base_code_debug.exe
 
 rem echo Web build
-rem C:\dev\odin\dev-master\odin build src ^
+rem odin build src ^
 rem     -target:js_wasm32 ^
-rem     -show-timings
+rem     -show-timings || goto :error
 
-if NOT "%1"=="all" goto End
+if NOT "%1"=="all" goto end
 
 echo Release build
-C:\dev\odin\dev-master\odin build src ^
-    -out:build/base_code_release.exe ^
+odin build src ^
+    -out:build/base_code.exe ^
     -build-mode:exe ^
     -subsystem:windows ^
     -o:speed ^
     -disable-assert ^
-    -show-timings
+    -show-timings || goto :error
 
-:End
+goto :end
+
+:error
+echo Failed with error #%errorlevel%
+exit /b %errorlevel%
+
+:end
